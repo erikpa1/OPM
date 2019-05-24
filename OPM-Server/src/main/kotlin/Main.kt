c480f9d02eb77a3793b737a7f5f97dfe2906da3e
erikpa1 <erikpa1@azet.sk>
2019-05-25 01:40:40 +0200

    update

--- OPM-Server/src/main/kotlin/Main.kt
+++ OPM-Server/src/main/kotlin/Main.kt
@@ -1,16 +1,22 @@
 import io.ktor.application.*
 import io.ktor.http.*
+import io.ktor.http.content.default
+import io.ktor.http.content.files
+import io.ktor.http.content.resources
 import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import java.io.File
 
 
-fun main(args: Array<String>) {
-    val server = embeddedServer(Netty, port = 8082) {
+fun main1(args: Array<String>) {
+    val server = embeddedServer(Netty, port = 8083) {
         routing {
+
             get("/") {
-                call.respondText("Hello World!", ContentType.Text.Plain)
+                //call.respondText("<h1>Hello World!</h1>", ContentType.Text.Plain)
+                call.respondRedirect("http://localhost:8083/index.html", true);
             }
             get("/demo") {
                 call.respondText("HELLO WORLD!")
@@ -19,3 +25,21 @@ fun main(args: Array<String>) {
     }
     server.start(wait = true)
 }
+
+fun main(args: Array<String>) {
+
+    val server = embeddedServer(Netty, port = 8083) {
+        routing {
+
+            get("/") {
+                //call.respondText("<h1>Hello World!</h1>", ContentType.Text.Plain)
+                call.respondRedirect("index.html", true);
+            }
+            get("/demo") {
+                call.respondText("HELLO WORLD!")
+            }
+        }
+    }
+    server.start(wait = true)
+}
+
